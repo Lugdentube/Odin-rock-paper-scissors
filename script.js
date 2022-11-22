26187b3520f321828b1fcd63e9f9230b3b3ef8a9
Lugdentube <samhayes1020@gmail.com>
2022-11-22 11:32:45 -0800

    write javascript to recieve inputs and display results

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+//set initial score. user score is first, computer score is second.
+let score = [0,0];
+
+//grab elements that will display results and score
+let resultMessage = document.querySelector('.results');
+let scoreMessage = document.querySelector('.score');
+
+//play a round in response to button click
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    let userChoice = button.textContent.toLowerCase();
+    let computerChoice = getComputerChoice();
+    let result = playRound(userChoice,computerChoice);
+    score = adjustScores(result,score);
+    scoreMessage.textContent = `THE SCORE IS USER:${score[0]} COMPUTER:${score[1]}`
+
+    if (score[0] === 5) {
+      resultMessage.textContent = 'Congratulations! You won the game.';
+      score = [0,0];
+    } else if (score[1] === 5) {
+      resultMessage.textContent = 'The computer won the game. Better luck next time.'
+      score = [0,0];
+    } else {
+      resultMessage.textContent  = `${result}`;
+    }
+  });
+});
+
 //we need to determine the computers selection
 function getComputerChoice() {
   //Define ranNum as a random integer between 0 and 2
@@ -24,64 +53,37 @@ function getComputerChoice() {
 
 //we need to determine the winner of a round
 function playRound(userChoice, computerChoice) {
-  //make input case insensitive
-  userChoice = userChoice.toLowerCase();
-
   //determine message to be displayed based on userChoice and computerChoice
   let message;
   if (userChoice === computerChoice) {
-    message = `You draw! You both played ${userChoice}`;
+    message = `You draw this round! You both played ${userChoice}`;
   } else if (userChoice === 'rock' && computerChoice === 'paper') {
-    message = 'You lose! Paper covers your rock.';
+    message = "You lose this round! Computer's paper covers your rock.";
   } else if (userChoice === 'rock' && computerChoice === 'scissors') {
-    message = 'You win! Your rock smashes scissors.';
+    message = "You win this round! Your rock smashes computer's scissors.";
   } else if (userChoice === 'paper' && computerChoice === 'rock') {
-    message = 'You win! Your paper covers rock.';
+    message = "You win this round! Your paper covers Computer's rock.";
   } else if (userChoice === 'paper' && computerChoice === 'scissors') {
-    message = 'You lose! Scissors cuts your paper.';
+    message = "You lose this round! Computer's scissors cuts your paper.";
   } else if (userChoice === 'scissors' && computerChoice === 'rock') {
-    message = 'You lose! Rock smashes your scissors.';
+    message = "You lose this round! Computer's rock smashes your scissors.";
   } else if (userChoice === 'scissors' && computerChoice === 'paper') {
-    message = 'You win! Your scissors cut paper.';
+    message = "You win this round! Your scissors cut computer's paper.";
   } else {
     message = 'error during playRound function. invalid user or computer choice';
   }
   return message;
 }
 
-//We need to play a game of 5 rounds
-function game() {
-  let userScore = 0;
-  let computerScore = 0;
-  
-  for (let i = 0; i < 5; i++) {
-    //determine choices and play round
-    let computerChoice = getComputerChoice();
-    let userChoice = prompt("Rock, paper, or scissors?");
-    result = playRound(userChoice,computerChoice);
-    //print the result message to the console
-    console.log(result);
-    
-    //adjust userScore and Computer score based on result message
-    switch (result.charAt(4)) {
-      case "w":
-        userScore++;
-      break;
-      case "l":
-        computerScore++;
-      break;
-    }
-    
-    //print the total score so far to the console
-    console.log(`User:${userScore} Computer:${computerScore}`);
+// returns adjusted score array based on results
+function adjustScores(result,score) {
+  switch (result.charAt(4)) {
+    case "w":
+      score[0]++;
+    break;
+    case "l":
+      score[1]++;
+    break;
   }
-
-  //print final results to the console
-  if (userScore > computerScore) {
-    console.log('User wins the game!');
-  } else if (computerScore > userScore) {
-    console.log('Computer wins the game!');
-  } else if (computerScore === userScore) {
-    console.log('Draw! we are all losers here.')
-  }
-}
+  return score;
+} 
